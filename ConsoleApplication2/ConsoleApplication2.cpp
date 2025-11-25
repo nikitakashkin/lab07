7a91cdb38a60560e41837c8db00ae23154103621
g <g>
2025-11-25 15:54:54 +0100

    Zadanie 1

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,20 +1,27 @@
-// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>	
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+void choinka(int n, int i = 0);
+
+int main() {//zadanie 1
+	int n;
+	cin >> n;
+	choinka(n);
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void choinka(int n, int i) {
+	if (i <= n) {
+		for (int a = 0; a <= (n-i+i/2); a++) {
+			cout << " ";
+		}
+		for (int a = 0; a < i; a++) {
+			cout << "*";
+		}
+		cout << endl;
+		choinka(n, i + 1);
+	}
+}
